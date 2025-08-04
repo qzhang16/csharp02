3a96270b697fc9a468bc0ecc3fa38fad77069240
qzhang16 <qzhang16@ra.rockwell.com>
2025-08-04 22:46:39 +0800

    afd

--- Class2/Class1.cs
+++ Class2/Class1.cs
@@ -2,5 +2,10 @@
 
 public class Class1
 {
+    public string Name { get; set; }
 
+    public void Ace01()
+    {
+        Console.WriteLine("hello from " + Name);
+    }
 }
--- app01/Program.cs
+++ app01/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Class2;
+
 Console.WriteLine("Hello, World!");
+Class1 class1 = new Class1 { Name = "as" };
+class1.Ace01();
